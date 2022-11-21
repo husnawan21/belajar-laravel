60f12e479ea78a74e888efe1e3c0e26ec190f7b1
Dwi Husnawan <husnawandwi@gmail.com>
2022-11-21 20:45:40 +0700

    search data dan pagination beserta withQueryString

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -17,7 +17,7 @@
           </div>
         </div>
       @endif
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between mt-12">
         <div class="flex items-center">
           <span class="font-bold text-2xl md:text-2xl text-base-content inline-block">Student List</span>
         </div>
@@ -26,6 +26,24 @@
             href="student-add" class="btn btn-sm sm:btn-md">Add data</a>
         </div>
       </div>
+
+      {{-- search start --}}
+      <form action="/students">
+        <div class="form-control mt-6">
+          <div class="input-group">
+            <input type="text" placeholder="Search…" class="input input-bordered w-full" name="keyword"
+              value="{{ request('keyword') }}" />
+            <button class="btn btn-square" type="submit">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </form>
+      {{-- search end --}}
       {{-- table --}}
       <div class="overflow-x-auto">
         <table class="table w-full rounded-lg shadow-lg">
@@ -36,6 +54,7 @@
               <th class="text-center">Name</th>
               <th class="text-center">Gender</th>
               <th class="text-center">NIS</th>
+              <th class="text-center">Class</th>
               <th class="text-center">Action</th>
             </tr>
           </thead>
@@ -47,6 +66,7 @@
                 <td class="capitalize">{{ $data->name }}</td>
                 <td class="text-center">{{ $data->gender }}</td>
                 <td class="text-center">{{ $data->nis }}</td>
+                <td class="text-center">{{ $data->class->name }}</td>
                 <td>
                   <a href="student/{{ $data->id }}" class="mx-1 btn btn-xs btn-outline">Detail</a>
                   <a href="student-edit/{{ $data->id }}" class="mx-1 btn btn-xs btn-outline">Edit</a>
@@ -66,6 +86,11 @@
         </table>
       </div>
       <!-- ... -->
+
     </article>
+    <div class="flex justify-center my-8">
+      {{ $studentList->links() }}
+    </div>
+    <h3 class="prose-h1">Haloooo kamu</h3>
   </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
